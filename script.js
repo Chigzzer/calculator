736b8c1374a2f6dad3630b4ab82a3cf20d5e75d5
Chigzzer <42282981+Chigzzer@users.noreply.github.com>
2022-12-16 09:56:21 +0000

    Added basic functions for the four operations

--- script.js
+++ script.js
@@ -1,1 +1,36 @@
-console.log('test');
+function add(a,b){
+    return parseFloat(a) + parseFloat(b);
+}
+function multiply(a,b){
+    return parseFloat(a) * parseFloat(b);
+}
+function subtract(a,b){
+    return parseFloat(a) - parseFloat(b);
+}
+function divide(a,b){
+    return parseFloat((parseFloat(a) / parseFloat(b)).toFixed(2));
+}
+
+
+function operate(a, b, operator){
+    let ans;
+    switch (operator){
+        case '+':
+            ans= add(a,b);
+            break;
+        case '*':
+            ans = multiply(a,b);
+            break;
+        case '-':
+            ans = subtract(a,b);
+            break;
+        case '/':;
+            ans = divide(a,b);
+            break;
+    }
+    console.log(ans);
+}
+(operate(5,7,'+'));
+(operate(5,7,'*'));
+(operate(5,7,'-'));
+(operate(5,7,'/'));
